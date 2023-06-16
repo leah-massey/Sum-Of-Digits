cc8297bd6df795a7d2bc75b8fa0098004a687a00
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-06-16 20:32:37 +0100

    Refactoring of code.

--- digitalRoot.js
+++ digitalRoot.js
@@ -1,23 +1,13 @@
 const digitalRoot = function (value) {
-  let sum = 0; //17
-  let sumArray;
-  let valueArray = value.toString().split(""); //["2","4","5","6"]
-  console.log(valueArray);
+  let sum = value;
+  let valueArray = value.toString().split("");
 
-  // while (valueArray.length > 1) {
-  valueArray.forEach((number) => {
-    sum += +number;
-  });
-  console.log(valueArray.length);
-  sumArray = sum.toString().split("");
-  console.log(valueArray.length);
-
-  while (sumArray.length > 1) {
+  while (valueArray.length > 1) {
     sum = 0;
-    sumArray.forEach((number) => {
+    valueArray.forEach((number) => {
       sum += +number;
     });
-    sumArray = sum.toString().split("");
+    valueArray = sum.toString().split("");
   }
 
   return sum;
